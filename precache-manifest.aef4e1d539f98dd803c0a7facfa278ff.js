67c32733109ae20d537cf0b74f20c91ac9556ed3
MrRav3n <dawidmrraven@gmail.com>
2019-10-12 14:04:49 +0200

    Updates

---
+++ precache-manifest.aef4e1d539f98dd803c0a7facfa278ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5eafc57e87332c9ccd9",
+    "url": "/Decentralized-ToDo/static/js/runtime~main.e5eafc57.js"
+  },
+  {
+    "revision": "92c164364899b9932430",
+    "url": "/Decentralized-ToDo/static/js/main.92c16436.chunk.js"
+  },
+  {
+    "revision": "9dcec5c617abb8aa4f66",
+    "url": "/Decentralized-ToDo/static/js/1.9dcec5c6.chunk.js"
+  },
+  {
+    "revision": "92c164364899b9932430",
+    "url": "/Decentralized-ToDo/static/css/main.513bfcff.chunk.css"
+  },
+  {
+    "revision": "9dcec5c617abb8aa4f66",
+    "url": "/Decentralized-ToDo/static/css/1.3f716daa.chunk.css"
+  },
+  {
+    "revision": "4604bab4d26480c9e4711b2ad5463b9e",
+    "url": "/Decentralized-ToDo/index.html"
+  }
+];
